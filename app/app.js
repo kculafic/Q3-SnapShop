84595762c572a9f28dddb75b05aa52d2e791f6ea
kculafic <kculafic@gmail.com>
2016-11-03 13:35:11 -0700

    catalog controller initial setup with functional cart-adding

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 import angular from 'angular'
 import angularMaterialize from 'angular-materialize'
 
+import CatalogCtrl from './catalog/catalog.controller'
 
-angular.module('my-app', [angularMaterialize]);
+
+angular.module('my-app', [angularMaterialize])
+  .controller('CatalogCtrl', CatalogCtrl);
---
+++ app/catalog/catalog.controller.js
@@ -0,0 +1,94 @@
+class CatalogCtrl {
+  constructor() {
+    this.cart = [];
+    this.subtotal = 0;
+    this.cameras= [
+    {
+      title: 'Nikon D3100 DSLR',
+      image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
+      rating: 4.1,
+      price: 369.99,
+      onSale: true
+    },
+    {
+      title: 'Canon EOS 70D',
+      image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
+      rating: 2.9,
+      price: 1099.0,
+      onSale: false
+    },
+    {
+      title: 'Nikon D810A',
+      image:'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
+      rating: 3.4,
+      price: 3796.95,
+      onSale: true
+    },
+    {
+      title: 'Leica M6',
+      image:'https://d1jqecz1iy566e.cloudfront.net/large/bf038.jpg',
+      rating: 5,
+      price: 7299.95,
+      onSale: false
+    },
+    {
+      title: 'Nikon D7200',
+      image:'http://cdn.mos.cms.futurecdn.net/ef9f6ec0a94a4ddaa501e73edcb46da8-650-80.jpg',
+      rating: 3.8,
+      price: 831.95,
+      onSale: false
+    },
+    {
+      title: 'Panasonic LX100',
+      image:'http://cdn.mos.cms.futurecdn.net/51b3bea2fd56f3b5fe281cf1d2f4965c-650-80.jpg',
+      rating: 4.8,
+      price: 500.00,
+      onSale: true
+    },
+    {
+      title: 'Fujifilm X-A2',
+      image:'http://www.adorama.com/images/Large/ifjxa2sl.jpg',
+      rating: 3.5,
+      price: 549.00,
+      onSale: false
+    },
+    {
+      title: 'Canon EOS-5D',
+      image:'http://www.adorama.com/images/Large/ica5dm4k.jpg',
+      rating: 5,
+      price: 2489.00,
+      onSale: false
+    },
+    {
+      title: 'Nikon 1-J5',
+      image:'http://www.adorama.com/images/Large/ink1j5wt3.jpg',
+      rating: 3.6,
+      price: 879.00,
+      onSale: false
+    },
+    {
+      title: 'Panasonic DMC-GM5',
+      image:'http://www.adorama.com/images/Large/ipcdmcgm5r.jpg',
+      rating: 4.2,
+      price: 499.00,
+      onSale: false
+    },
+    {
+      title: 'Canon EOS M10',
+      image:'http://www.adorama.com/images/Large/icam10k.jpg',
+      rating: 3.8,
+      price: 449.00,
+      onSale: true
+    }
+  ]
+  }
+
+  addCamera(camera, price) {
+
+    this.cart.push({
+      title: camera,
+      price: price
+    })
+  }
+}
+export default CatalogCtrl;
