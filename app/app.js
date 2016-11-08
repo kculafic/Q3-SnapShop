2a4a68857f6a75fb7674a4b4d8b086a17126414c
kculafic <kculafic@gmail.com>
2016-11-08 10:42:41 -0800

    Still functional, last commit before routing with uiRouter

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 import angular from 'angular'
 import angularMaterialize from 'angular-materialize'
 
+import uiRouter from 'angular-ui-router'
+
+
 import CatalogCtrl from './catalog/catalog.controller'
 import CatalogService from './catalog/catalog.service';
 
 
-angular.module('my-app', [angularMaterialize])
+
+
+angular.module('my-app', [angularMaterialize, uiRouter])
   .service('CatalogService', CatalogService)
   .controller('CatalogCtrl', CatalogCtrl);
